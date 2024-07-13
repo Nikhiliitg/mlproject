4b781735772f63f0917b6a1170eda3237ab3f591
Nikhiliitg <d.nikhil@op.iitg.ac.in>
2024-07-13 20:44:26 +0530

    Add .DS_Store to .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Nikhil',
+    author_email='d.nikhil@op.iitg.ac.in',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn'],
+    
+    
+)
