dfa584cb9b6d671cbdc25adfcb1b2bb162289838
Nikhiliitg <d.nikhil@op.iitg.ac.in>
2024-07-15 10:30:35 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirment(file_path:str)->List[str]:
+    '''This function will return list of requirment'''
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        requirments=[req.replace("\n","")for req in requirments]
+        
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+    return requirments
 
 setup(
     name='mlproject',
@@ -6,7 +19,7 @@ setup(
     author='Nikhil',
     author_email='d.nikhil@op.iitg.ac.in',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn'],
+    install_requires=get_requirment('requirement.txt')
     
     
 )
